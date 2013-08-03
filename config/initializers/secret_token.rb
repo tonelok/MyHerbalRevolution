12ef5a6b016852a65d1e9ab8be2d7ca814430a4f
Tony <tonygrerro@gmail.com>
2013-08-02 20:27:26 -0700

    Created a Rails Application and adddes status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyHerbalRevolution::Application.config.secret_token = 'b54feec38d8b96be82acd444aea4bfee64d2d5d5091572bd29312c01e49a20e181ae7e18ad76cdb9c2fe06fcd5a95dd4da6fd16b99305c387b4859bfe8df0860'
